ef5c51066c2a39b253ec97d7b3031af90aca5c58
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-07 13:44:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
   return (
     <div>
       <Router>
-                <Header />
-                <Routes>
-                <Route path="/" element={<Home />} />
-                </Routes>
-                <Footer />
-            </Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
