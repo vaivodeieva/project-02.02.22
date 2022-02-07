8657bbc341553ffa02dbf76621e4f4c1591708ea
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-07 13:43:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      
+      <Router>
+                <Header />
+                <Routes>
+                <Route path="/" element={<Home />} />
+                </Routes>
+                <Footer />
+            </Router>
     </div>
   );
 }
