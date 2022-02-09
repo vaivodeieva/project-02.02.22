455f0eed78e032f0918971760ec53c68f4e10c53
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-09 12:55:59 +0200

    update tours

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <header>
                 <h2><a href="#">Life is a party!</a></h2>
                 <nav>
-                    <li><a href="#">Tours</a></li>
+                    <li><a href="/tours">Tours</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </nav>
