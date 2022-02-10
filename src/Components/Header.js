ac2f90098a354a6a074c886bc8c09c6dbd82ff1c
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-10 16:27:39 +0200

    cleaning code

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,9 @@ function Header() {
     return (
         <div>
             <header>
-                <h2><a href="#">Life is a party!</a></h2>
+                <h2><a href="/">Life is a party!</a></h2>
                 <nav>
-                    <li><a href="tours">Tours</a></li>
+                    <li><a href="/tours">Tours</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </nav>
