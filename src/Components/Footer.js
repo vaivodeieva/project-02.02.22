24255ccd519f39b5459ab584bf4ae8ab81bfc074
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-12 14:06:01 +0200

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
 function Footer() {
-   
+
     return (
         <div>
-
+            <footer>
+                <p>Images courtesy of <a href="http://unsplash.com/">unsplash</a>.</p>
+                <p>Why are you even reading this?! There's never anything interesting in the footer!</p>
+                <ul>
+                    <li><a href="#"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+                    <li><a href="#"><i class="fa fa-snapchat-square fa-2x"></i></a></li>
+                </ul>
+            </footer>
         </div>
     )
 }
