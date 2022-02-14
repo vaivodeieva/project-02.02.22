c479e845b6286247198a22394f36c723e53984cc
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-14 22:15:47 +0200

    Update Tours.js

--- src/Views/Tours.js
+++ src/Views/Tours.js
@@ -51,18 +51,7 @@ function Tours() {
                                 <input type="number" name="num_adults" id="num_adults" min="1" max="10" step="1" />
 
 
-                                {/* <select name="num_adults" id="num_adults">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                    <option value="6">6</option>
-                                    <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                </select> */}
+                        
 
                             </div>
 
