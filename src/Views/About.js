b4f55e1ce662a76f58528726229bc2bbd0efbe24
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-17 12:44:07 +0200

    updates

--- src/Views/About.js
+++ src/Views/About.js
@@ -3,16 +3,9 @@ function About() {
 
     return (
         <div>
-            <ul class="ca-menu">
-                <li>
-                    <a href="#" />
-                    <div class="ca-content"></div>
-                    <h2 class="ca-main">Acecoderz</h2>
-                    <h3 class="ca-sub">Powering Hackinux</h3>
-                </li>
-            </ul>
 
         </div>
+
     )
 }
 
