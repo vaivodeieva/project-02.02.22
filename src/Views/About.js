efcd67eabd5b9afc41e333a2d33c943646d1a3fe
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-02-21 22:48:09 +0200

    Update About.js

--- src/Views/About.js
+++ src/Views/About.js
@@ -11,10 +11,6 @@ function About() {
 <div className="top-container">
 <div className="bgimg1 w3-display-container w3-grayscale-min" id="home">
   <div className="w3-display-left w3-text-white" style={{padding:'48px'}}>
-    {/* <span className="w3-jumbo w3-hide-small">Start something that matters</span><br/> */}
-    {/* <span className="w3-xxlarge w3-hide-large w3-hide-medium">Start something that matters</span><br/> */}
-    {/* <span className="w3-large">Stop wasting valuable time with projects that just isn't you.</span> */}
-    {/* <p><a href="#about" className="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Learn more and start today</a></p> */}
   </div>
   <div className="w3-display-bottomleft w3-text-grey w3-large" style={{padding: '24px 48px'}}>
     <i className="fa fa-facebook-official w3-hover-opacity"></i>
