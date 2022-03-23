b5bf3ed185911846ab7f7a6912f9e2c59068ad29
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-03-23 17:29:45 +0200

    underline active header link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,26 @@
 import React from "react";
+import '../Assets/CSS/Header.css';
+import $ from 'jquery';
+
 function Header() {
+    $(function(){
+        // this will get the full URL at the address bar
+        let url = window.location.href; 
+    
+        // passes on every "a" tag 
+        $("#links li a").each(function() {
+                // checks if its the same on the address bar
+            if(url === (this.href)) { 
+                $(this).closest("li a").addClass("active");
+            }
+        });
+    });
 
     return (
         <div>
             <header>
                 <h2><a href="/">Life is a party!</a></h2>
-                <nav>
+                <nav id="links">
                     <li><a href="/tours">Tours</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
@@ -16,3 +31,4 @@ function Header() {
 }
 
 export default Header;
+
