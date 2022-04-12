71428566b7dbdfbd39977a84fb5d70cbf53fc233
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-04-12 15:31:21 +0300

    Update Contacts.js

--- src/Views/Contacts.js
+++ src/Views/Contacts.js
@@ -3,6 +3,19 @@ function Contacts() {
 
     return (
         <div>
+            <div className="container">
+                <div className="quote">
+                    <h1>Get A Quote</h1>
+                    <p>Fill out the form and our team will get back to you within 24 hours.</p>
+                </div>
+                <form action="">
+                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Email" />
+                    <textarea name="Message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                    <button type="submit">Send Message</button>
+                </form>
+
+            </div>
 
         </div>
     )
