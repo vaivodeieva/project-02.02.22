e09bb66097115b102339d705d0b58df550d7297b
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-04-12 15:46:13 +0300

    update contacts

--- src/Views/Contacts.js
+++ src/Views/Contacts.js
@@ -4,18 +4,19 @@ function Contacts() {
 
     return (
         <div>
-            <div className="container">
+            <div className="container-contacts">
+            <div className="inner-container-contacts">
                 <div className="quote">
-                    <h1>Get A Quote</h1>
-                    <p>Fill out the form and our team will get back to you within 24 hours.</p>
+                    <h1 className="contacts-h1">Get A Quote</h1>
+                    <p className="contacts-p">Fill out the form and our team will get back to you within 24 hours.</p>
                 </div>
-                <form action="">
-                    <input type="text" placeholder="Name" />
-                    <input type="text" placeholder="Email" />
-                    <textarea name="Message" id="" cols="30" rows="10" placeholder="Message"></textarea>
-                    <button type="submit">Send Message</button>
+                <form className="contacts-form" action="">
+                    <input className="contacts-input" type="text" placeholder="Name" />
+                    <input className="contacts-input" type="text" placeholder="Email" />
+                    <textarea className="textarea-contacts" name="Message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                    <button className="contacts-button" type="submit">Send Message</button>
                 </form>
-
+              </div>
             </div>
 
         </div>
