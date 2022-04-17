126f930a2a90e0b5806d768796d48878f15bfc6c
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-04-17 18:57:44 +0300

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,75 +4,75 @@ function Home() {
 
     return (
         <div>
-            <section class="hero-home">
-                <div class="background-image"></div>
-                <div class="hero-content-area">
+            <section className="hero-home">
+                <div className="background-image"></div>
+                <div className="hero-content-area">
                     <h1>Life is a party!</h1>
                     <h3>Unmissable Adventure Tours With Your Friends</h3>
-                    <a href="#" class="btn">Contact Us Now</a>
+                    <a href="#" className="btn">Contact Us Now</a>
                 </div>
             </section>
 
-            <section class="destinations">
-                <h3 class="title">Some of our destinations:</h3>
+            <section className="destinations">
+                <h3 className="title">Some of our destinations:</h3>
                 <p>Tired of the beach alone? Are the plains too plain? Come along with us on one of our unusual adventures with yout friends. Here are some pictures from people who have had elevated experiences with us.</p>
                 <hr />
 
-                <ul class="grid">
-                    <li class="small image-1"></li>
-                    <li class="large image-2"></li>
-                    <li class="large image-3"></li>
-                    <li class="small image-4"></li>
+                <ul className="grid">
+                    <li className="small image-1"></li>
+                    <li className="large image-2"></li>
+                    <li className="large image-3"></li>
+                    <li className="small image-4"></li>
                 </ul>
             </section>
 
-            <section class="packages">
-                <h3 class="title">Tour Packages</h3>
+            <section className="packages">
+                <h3 className="title">Tour Packages</h3>
                 <p>We offer a variety of group (minimum 5 people) packages. Whether you've spent some summers together or this might be your first adventure, we've got the perfect vacation for you.</p>
                 <hr />
 
-                <ul class="grid">
+                <ul className="grid">
                     <li>
-                        <i class="fa fa-compass fa-4x"></i>
+                        <i className="fa fa-compass fa-4x"></i>
                         <h4>Guided Trips</h4>
                         <p>Looking for the complete experience? Take a tour with one of our experts. They'll show you secrets that you're likely to miss otherwise.</p>
                     </li>
                     <li>
-                        <i class="fa fa-camera-retro fa-4x"></i>
+                        <i className="fa fa-camera-retro fa-4x"></i>
                         <h4>Photo Trips</h4>
                         <p>Want to experience nature's beauty without all of that annoying exercise? Take a photo tour on one of our <em>Life is a party!</em> buses.</p>
                     </li>
                     <li>
-                        <i class="fa fa-bicycle fa-4x"></i>
+                        <i className="fa fa-bicycle fa-4x"></i>
                         <h4>Biking Trips</h4>
                         <p>If bicycles are more your speed, consider taking a tour through one of our mountain or city bike paths. We'll provide the bikes, and lunch too!</p>
                     </li>
                     <li>
-                        <i class="fa fa-flag-checkered fa-4x"></i>
+                        <i className="fa fa-flag-checkered fa-4x"></i>
                         <h4>Racing Trips</h4>
                         <p>Got a competitive spirit? Sign up for one of our challenge-based marathons! Try to reach the summit before any other group.</p>
                     </li>
                 </ul>
             </section>
 
-            <section class="testimonials">
-                <h3 class="title">Testimonials from our adventurers:</h3>
+            <section className="testimonials">
+                <h3 className="title">Testimonials from our adventurers:</h3>
                 <hr />
-                <p class="quote">Wow! This tour made me realize how much I love being outside with my friends. After going on one of these tours, I can safely say that beer pong is my favorite game all time, also the cultural programs were really interesting!</p>
-                <p class="author">- Albert Herter</p>
-                <p class="quote">Wow, this really blew my mind. We had so much fun at the beach, and also some hidden secrets revealed. Come on, I'm living in this city for 5 years. Amazing!</p>
-                <p class="author">- Sharon Rosenberg</p>
-                <p class="quote">If you want to understand your friends better, head to the mountains. I mean, seriously. It's like sitting next to a campfire and just talk in the sunset, woah. You know? It's like that.</p>
-                <p class="author">- Luis Mendoza</p>
+                <p className="quote">Wow! This tour made me realize how much I love being outside with my friends. After going on one of these tours, I can safely say that beer pong is my favorite game all time, also the cultural programs were really interesting!</p>
+                <p className="author">- Albert Herter</p>
+                <p className="quote">Wow, this really blew my mind. We had so much fun at the beach, and also some hidden secrets revealed. Come on, I'm living in this city for 5 years. Amazing!</p>
+                <p className="author">- Sharon Rosenberg</p>
+                <p className="quote">If you want to understand your friends better, head to the mountains. I mean, seriously. It's like sitting next to a campfire and just talk in the sunset, woah. You know? It's like that.</p>
+                <p className="author">- Luis Mendoza</p>
             </section>
 
-            <section class="contact">
-                <h3 class="title">Learn more</h3>
+            <section className="contact">
+                <h3 className="title">Learn more</h3>
                 <p>Want to know about our upcoming events, or come to one of our mixers? Just sign up for our mailing list. No spam from us, we promise! Except for the spam we give you to keep up your energy while you're having fun with your friends. Have a blast! We have tons of that.</p>
                 <hr />
                 <form>
                     <input type="email" placeholder="Email" />
-                    <a href="#" class="btn">Subscribe now</a>
+                    <a href="#" className="btn">Subscribe now</a>
                 </form>
             </section>
         </div>
